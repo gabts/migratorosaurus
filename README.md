979964124710512ab564b0d9ad22fa0c3cc5df44
gabbes <gabtost@gmail.com>
2019-06-06 23:35:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# pgup
+
+A very simple pg database schema upgrade tool.
+
+## Install
+
+```sh
+npm install pgup
+# or
+yarn add pgup
+```
+
+## Sample usage
+
+Create database migration files. Defaultly pgup will look for `./sql/` directory, but this can be configured. The file naming convention should be _index_-_name_.sql. A valid migration path would be: `./sql/1-initial-migration.sql`.
+
+```sql
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (100) NOT NULL
+);
+```
+
+And add a pgup step after initializing your pg instance.
+
+```js
+import { Pool } from 'pg';
+import { pgup } from 'pgup';
+
+const pool = new Pool();
+
+(async () => {
+  await pgup(pool);
+})();
+```
+
+## Configuration
+
+The first argument is a [pg](https://www.npmjs.com/package/pg) Pool instance and is required.
+
+The second argument is options but allows you configure pgup.
+
+- `directory` A string value of the path to the directory containing your postgres database migrations in .sql files. Default is "sql".
+
+## Lisence
+
+[MIT](./LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Pool } from 'pg';
+
+interface Args {
+  directory?: string;
+}
+
+export async function pgup(pool: Pool, args: Args = {}): Promise<void> {
+  const { directory = 'sql' } = args;
+
+  const client = await pool.connect();
+
+  // Check if migrations table exists
+  const migrationTableQueryResult = await client.query(`
+    SELECT COUNT (*)
+    FROM information_schema.tables
+    WHERE table_name = 'migrations';
+  `);
+
+  // If a migrations table does not exist
+  if (migrationTableQueryResult.rows[0].count !== '1') {
+    console.log('pgup: No migrations table found. Creating one...');
+
+    await client.query(`
+      CREATE TABLE migrations (
+        id serial PRIMARY KEY,
+        file VARCHAR (100) UNIQUE NOT NULL
+      );
+    `);
+
+    console.log('pgup: Migrations table was created successfully.');
+  }
+
+  const migrationsQueryResult = await client.query(`
+    SELECT file
+    FROM migrations
+    ORDER BY file ASC;
+  `);
+
+  const migrationHistoryFiles = migrationsQueryResult.rows.map((row) => {
+    return row.file;
+  });
+
+  const dir = path.resolve(directory);
+
+  // Read files inside migrations directory, filter any non .sql files and sort
+  // them according to their index prefix in ascending order. Lastly add a
+  // property whether the they have already been migrated.
+  const files = fs
+    .readdirSync(dir)
+    .filter((file) => file.match(/\.sql$/))
+    .sort((fileA, fileB) => {
+      const a = parseInt(fileA.split('-')[0], 10);
+      const b = parseInt(fileB.split('-')[0], 10);
+
+      return a > b ? 1 : a < b ? -1 : 0;
+    })
+    .map((file) => ({ file, migrated: migrationHistoryFiles.includes(file) }));
+
+  // Loop through each migration file
+  for (const { file, migrated } of files) {
+    // Return if file has already been migrated
+    if (migrated) {
+      return;
+    }
+
+    console.log(`pgup: Found new migration "${file}". Upgrading...`);
+
+    // Read migration sql from file
+    const sql = fs.readFileSync(`${dir}/${file}`, 'utf8');
+
+    // Execute sql
+    await client.query(sql);
+
+    // Store migration in migrations table
+    await client.query(`
+      INSERT INTO migrations (
+        file
+      ) VALUES (
+        '${file}'
+      );
+    `);
+
+    console.log(`pgup: "${file}" was upgraded successfully.`);
+  }
+
+  client.release();
+}
